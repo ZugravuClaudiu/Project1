b88ad2a0aad6edfc3bd4d4c85d9e1c7240058f25
Claudiu.Zugravu <claudiu.zugravu@theaccessgroup.com>
2023-06-26 12:29:25 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!-change");
+        System.out.println("Hello world!-change3");
+        System.out.println("Hello world!-change1");
+        System.out.println("Incercam commit si push pe branch1");
     }
 }
