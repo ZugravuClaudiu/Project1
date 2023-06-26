8bf40b10954b077ed60eb10440dc8b20b6195d29
Claudiu.Zugravu <claudiu.zugravu@theaccessgroup.com>
2023-06-26 12:33:54 +0300

    commit4

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Incercam commit si push pe branch1");
         System.out.println("Hello world!-change11");
         System.out.println("Sunt din nou pe master si fac commtul 3");
+        System.out.println("Alabalaportocala");
     }
 }
